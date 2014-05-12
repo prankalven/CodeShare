9473d045ea24f8acb0d6c044fc69e608a420c887
prankcatax <prankcatax@users.noreply.github.com>
2014-05-12 22:37:17 +0800

    Create article_author.php

---
+++ article_author.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>作者文章</title>
+</head>
+<body>
+<div class="container article-view">
+	<?php include("_content_nav.php") ?>	
+	<!-- content -->
+	<div class="content">
+
+		<h1> <?=$user->Account ?></h1>
+
+		<?php foreach($results as $article){ ?>
+		<table class="table table-bordered">
+			<tr>
+				<td width="50">標題</td>
+				<td>
+					<a href="<?=site_url("article/view/".$article->ArticleID)?>">
+					<?=htmlspecialchars($article->Title)?></a>
+				</td>
+			</tr>
+			<tr>
+				<td> 內容 </td>
+				<td><?=nl2br(htmlspecialchars($article->Content))?></td>
+			</tr>
+		</table>
+		<?php } ?>
+		<p>
+			<?=$pageLinks?>
+		</p>
+
+	</div>
+</div>
+</body>
+</html>
